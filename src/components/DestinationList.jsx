676f9026533d8ac7779dcb018001bf9fc49629a3
Stephanie Sanchez <stephk006@gmail.com>
2022-06-08 22:45:41 -0400

    added images to database, added date range picker

--- src/components/DestinationList.jsx
+++ src/components/DestinationList.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
-import { Col, Row, Card } from "antd";
+import { Col, Row, Card, Space, DatePicker } from "antd";
+
+const { RangePicker } = DatePicker;
 
 function DestinationList() {
   const [destinations, setDestinations] = useState();
@@ -16,10 +18,25 @@ function DestinationList() {
           <h2>Loading...</h2>
         ) : (
           destinations.map((destination) => (
-            <Col style={{ width: "300px" }} key={destination.id}>
-              <Card title={destination.name} hoverable>
-                <p>Start Date: {destination.startDate}</p>
-                <p>End Date: {destination.endDate}</p>
+            <Col
+              style={{ width: "300px", margin: "1.5em" }}
+              key={destination.id}
+            >
+              <Card
+                cover={
+                  <img
+                    alt={`Image representing ${destination.name}`}
+                    src={destination.image}
+                  />
+                }
+                hoverable
+              >
+                <Card.Meta title={destination.name} />
+                <p>
+                  <Space direction="vertical" description="RangePicker">
+                    <RangePicker />
+                  </Space>
+                </p>
                 <p>Status: {destination.status}</p>
               </Card>
             </Col>
