76b114277addc1f40bbfdd9c7f88c8761bbc19d7
Stephanie Sanchez <stephk006@gmail.com>
2022-06-14 22:05:12 -0400

    testing upload button from antd

--- src/components/AddDestination.jsx
+++ src/components/AddDestination.jsx
@@ -1,6 +1,27 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, message, Upload } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
+
+const props = {
+  name: "file",
+  action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+  headers: {
+    authorization: "authorization-text",
+  },
+
+  onChange(info) {
+    if (info.file.status !== "uploading") {
+      console.log(info.file, info.fileList);
+    }
+
+    if (info.file.status === "done") {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === "error") {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
 
 export default function AddDestination() {
   let navigate = useNavigate();
@@ -39,14 +60,17 @@ export default function AddDestination() {
           <Input />
         </Form.Item>
         <br />
-        <Form.Item
+        {/* <Form.Item
           name="image"
           type="url"
           label="Photo:"
           action="/action_page.php"
         >
           <input type="file" id="myFile" name="filename" />
-        </Form.Item>
+        </Form.Item> */}
+        <Upload {...props}>
+          <Button icon={<UploadOutlined />}>Click to upload</Button>
+        </Upload>
         <br />
         <Button type="primary" htmlType="submit">
           Submit
