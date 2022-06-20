0982eb89851d4de6a87863df23d011de239a652f
Stephanie Sanchez <stephk006@gmail.com>
2022-06-19 23:35:43 -0400

    added more fields to addDestination form

--- src/components/AddDestination.jsx
+++ src/components/AddDestination.jsx
@@ -7,8 +7,32 @@ export default function AddDestination() {
   let navigate = useNavigate();
   const [error, setError] = useState("");
 
-  const handleSubmit = ({ name, beginDate, endDate, image, status }) => {
-    const newDestination = { name, beginDate, endDate, image, status };
+  const handleSubmit = ({
+    name,
+    beginDate,
+    endDate,
+    image,
+    status,
+    gettingAround,
+    gettingAroundDetails,
+    gettingThere,
+    gettingThereDetails,
+    lodging,
+    lodgingDetails,
+  }) => {
+    const newDestination = {
+      name,
+      beginDate,
+      endDate,
+      image,
+      status,
+      gettingAround,
+      gettingAroundDetails,
+      gettingThere,
+      gettingThereDetails,
+      lodging,
+      lodgingDetails,
+    };
     fetch("https://keep-up-sks.web.app/destinations", {
       method: "POST",
       headers: {
@@ -43,6 +67,64 @@ export default function AddDestination() {
         <Form.Item name="image" type="url" label="Photo:">
           <Input
             placeholder={"Paste the url for your destination's picture here"}
+          />
+        </Form.Item>
+        <Form.Item
+          name="gettingThere"
+          type="text"
+          label="How will you get there?:"
+        >
+          <Input
+            placeholder={
+              "Enter mode of transportation. For example: car, plane, train, etc."
+            }
+          />
+        </Form.Item>
+        <Form.Item
+          name="gettingThereDetails"
+          type="text"
+          label="Getting There Details:"
+        >
+          <Input
+            placeholder={
+              "Enter details related to how you will get there. For example: airline, flight time, etc."
+            }
+          />
+        </Form.Item>
+        <Form.Item
+          name="gettingAround"
+          type="text"
+          label="How will you be getting around while there?:"
+        >
+          <Input
+            placeholder={
+              "Enter details related to how you will get around. For example: car rental, public transportation, chauffeur, etc."
+            }
+          />
+        </Form.Item>
+        <Form.Item
+          name="gettingAroundDetails"
+          type="text"
+          label="Getting Around Details:"
+        >
+          <Input
+            placeholder={
+              "Enter details related to how you will get there. For example: car rental company, car pickup & drop off time, etc."
+            }
+          />
+        </Form.Item>
+        <Form.Item name="lodging" type="text" label="Lodging:">
+          <Input
+            placeholder={
+              "Enter where you will be staying. For example: hotel, airbnb, etc."
+            }
+          />
+        </Form.Item>
+        <Form.Item name="lodgingDetails" type="" label="Lodging Details:">
+          <Input
+            placeholder={
+              "Enter details related to where you will be staying. For example: hotel name, address, etc."
+            }
           />
         </Form.Item>
         <br />
